0fa9345c3edf96813cc7ec4e35bcddf1b6744613
USER-PC\a1256 <a1256438@gmail.com>
2018-03-11 21:22:30 +0800

    1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 /**導入上層目錄的hero.ts */
+import { HEROES } from '../mock-heroes';
+/**導入上層目錄的mock-heroes */
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  /**hero.ts */
+  heroes = HEROES;
+  /**引用app/hero.ts */
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+  selectedHero: Hero;
+
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
   constructor() { }
 
   ngOnInit() {
